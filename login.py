21a2575031aeb7025a9c584801170b2aef250fa7
deepanshu097 <75848565+deepanshu097@users.noreply.github.com>
2021-07-12 02:49:36 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,16 +1,16 @@
-from tkinter_page import *
+from tkinter import *
 root = Tk()
 root.geometry("500x300")
 
 def getvals():
     print("accepted")
 
-Label(root, text = "registration form",font = "ar 15 bold").grid(row = 0, cloumn = 3)
+Label(root, text = "registration form",font = "ar 15 bold").grid(row = 0, column = 3)
 
 first_name = Label(root, text = "first name")
 last_name = Label(root, text = "last name")
 gender = Label(root, text = "gender")
-phone = label(root, text = "phone")
+phone = Label(root, text = "phone")
 payment_mode = Label(root, text = "payment mode")\
 
 first_name.grid(row=1,column=2)
@@ -23,20 +23,20 @@ first_namevalue = StringVar
 last_namevalue = StringVar
 gendervalue = StringVar
 phonevalue = StringVar
-payment_mode_namevalue = StringVar
+payment_modevalue = StringVar
 checkvalue = IntVar
 
 first_nameentry = Entry(root, textvariable =first_namevalue)
 last_nameentry = Entry(root, textvariable =last_namevalue)
 genderentry = Entry(root, textvariable =gendervalue)
-phoneentry = Entry(root, textvariable =phonevaluevalue)
+phoneentry = Entry(root, textvariable =phonevalue)
 payment_modeentry = Entry(root, textvariable =payment_modevalue)
 
 first_nameentry.grid(row=1,column=3)
 last_nameentry.grid(row=2,column=3)
 genderentry.grid(row=3,column=3)
 phoneentry.grid(row=4,column=3)
-payment_modeentryentry.grid(row=5,column=3)
+payment_modeentry.grid(row=5,column=3)
 
 checkbtn = Checkbutton(text="remember me?", variable=checkvalue)
 checkbtn.grid(row=6,column=3)
